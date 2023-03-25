1a9db5e0e87fef435d67dfe7a2219ed2b644173c
Saul P <saulpulido52@gmail.com>
2023-03-25 11:09:06 -0700

    ignorando archivo

---
+++ index.js
@@ -0,0 +1,12 @@
+// npm init -y ----> inicializar cualquier proyecto con node.js
+// node index.js ---> comando iniciar el node
+// npm install express ---> instalar librerias de node con las que se trabajara el proyecto
+
+
+
+// let nombre = "Saul"
+// let apellidos = "pulido"
+// let nombre_completo= nombre + " "+ apellidos
+let saludar = "Hola Mundo"
+
+console.log(saludar)
